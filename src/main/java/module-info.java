adc2749b4c27adcaad0bcdb5e5bff1f6e35cc945
Swaib Alif <ahmedswaib328@gmail.com>
2025-12-28 17:36:53 +0600

    store changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.busticketbooking {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.busticketbooking to javafx.fxml;
